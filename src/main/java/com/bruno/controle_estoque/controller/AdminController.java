d1812fd930446e0c89f6bff00a5797e4d5cecd56
Bruno <bruno7motadev@gmail.com>
2025-07-07 17:07:52 -0300

    feat: Iniciando configuração Log4j

--- src/main/java/com/bruno/controle_estoque/controller/AdminController.java
+++ src/main/java/com/bruno/controle_estoque/controller/AdminController.java
@@ -4,6 +4,7 @@ import com.bruno.controle_estoque.dto.request.PromoteRequestDTO;
 import com.bruno.controle_estoque.dto.response.UserResponseDTO;
 import com.bruno.controle_estoque.service.AdminService;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.nio.file.AccessDeniedException;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/admin")
 @PreAuthorize("hasRole('ADMIN')")
@@ -24,6 +26,9 @@ public class AdminController {
     @GetMapping("/users")
     public ResponseEntity<List<UserResponseDTO>> getAllUsers()
             throws AccessDeniedException {
+
+        log.info("Requisição GET recebida para buscar usuários na rota /api/admin/users");
+
         return ResponseEntity.status(HttpStatus.OK)
                 .body(adminService.getAllUsers());
     }
@@ -32,6 +37,9 @@ public class AdminController {
     public ResponseEntity<UserResponseDTO> getUserById(
             @PathVariable Long id
     ) throws AccessDeniedException {
+
+        log.info("Requisição GET recebida para buscar usuário por ID na rota /api/admin/users/{id}");
+
         return ResponseEntity.status(HttpStatus.OK)
                 .body(adminService.getUserById(id));
     }
@@ -40,6 +48,9 @@ public class AdminController {
     public ResponseEntity<?> promoteUser(
             @RequestBody @Valid PromoteRequestDTO dto
     ) throws AccessDeniedException {
+
+        log.info("Requisição PUT recebida na rota de /api/admin/promote. Usuário promovido: {}", dto.getUsername());
+
         adminService.promoteUser(dto);
         return ResponseEntity.status(HttpStatus.OK)
                 .build();
@@ -49,6 +60,9 @@ public class AdminController {
     public ResponseEntity<?> deleteUser(
             @PathVariable Long id
     ) throws AccessDeniedException {
+
+        log.info("Requisição DELETE recebida na rota /api/admin/delete/{id}. ID do usuário excluído: {}", id);
+
         adminService.deleteUser(id);
         return ResponseEntity.status(HttpStatus.OK)
                 .build();
--- src/main/java/com/bruno/controle_estoque/service/AdminService.java
+++ src/main/java/com/bruno/controle_estoque/service/AdminService.java
@@ -7,6 +7,7 @@ import com.bruno.controle_estoque.exceptions.InvalidRoleException;
 import com.bruno.controle_estoque.mapper.UserMapper;
 import com.bruno.controle_estoque.model.Users;
 import com.bruno.controle_estoque.repository.UsersRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Service;
 import java.nio.file.AccessDeniedException;
 import java.util.List;
 
+@Slf4j
 @Service
 public class AdminService {
 
@@ -31,6 +33,8 @@ public class AdminService {
             throw new AccessDeniedException("Apenas administradores podem listar usuários.");
         }
 
+        log.info("Admin '{}' fez uma requesição para buscar todos os usuários.", authenticatedUser.getUsername());
+
         return usersRepository.findAll()
                 .stream()
                 .map(UserMapper::toDTO)
@@ -47,6 +51,8 @@ public class AdminService {
         Users user = usersRepository.findById(id)
                 .orElseThrow(() -> new UsernameNotFoundException("Usuário de ID " + id + " não encontrado. Tente novamente!"));
 
+        log.info("Admin '{}' fez uma requesição para buscar por usuário. Buscou o usuário: '{}'", authenticatedUser.getUsername(), user.getUsername());
+
         return UserMapper.toDTO(user);
     }
 
@@ -55,7 +61,10 @@ public class AdminService {
         Users authenticatedUser = authService.getUserAuthenticated();
 
         if (authenticatedUser.getRole() != Roles.ADMIN) {
+
+            log.error("Usuário '{}', tentou promover '{}', para outros cargo, mas não tem permissão.", authenticatedUser.getUsername(), dto.getUsername());
             throw new AccessDeniedException("Apenas administradores podem promover usuários.");
+
         }
 
         Users user = usersRepository.findByUsername(dto.getUsername())
@@ -65,14 +74,23 @@ public class AdminService {
         try {
             newRole = Roles.valueOf(dto.getNewRole().toUpperCase());
         } catch (IllegalArgumentException ex) {
+
+            log.error("Admin '{}' tentou usar um role inexistente.", authenticatedUser.getUsername(), dto.getUsername());
             throw new InvalidRoleException("Role inválida: " + dto.getNewRole());
+
         }
 
-        if (user.getRole() == Roles.ADMIN && newRole != Roles.ADMIN)
+        if (user.getRole() == Roles.ADMIN && newRole != Roles.ADMIN) {
+            log.error("Admin '{}', tentou rebaixar o usuário admin '{}'", authenticatedUser.getUsername(), dto.getUsername());
             throw new InvalidRoleException("Não é permitido rebaixar um ADMIN.");
+        }
 
-        if (user.getRole() == newRole)
+        if (user.getRole() == newRole) {
+            log.error("Admin '{}', tentou promover o usuário '{}'", authenticatedUser.getUsername(), dto.getUsername());
             throw new InvalidRoleException("Usuário já possui o cargo " + newRole + ".");
+        }
+
+        log.info("Admin '{}', promoveu o usuário '{}'.", authenticatedUser.getUsername(), user.getUsername());
 
         user.setRole(newRole);
         usersRepository.save(user);
