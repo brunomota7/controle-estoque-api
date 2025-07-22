dab9262662174b19791012f3755a598cf6e13cbf
Bruno <bruno7motadev@gmail.com>
2025-07-22 16:54:37 -0300

    feat: Adicionado o Swagger para melhor visualização dos endpoints da aplicação

--- src/main/java/com/bruno/controle_estoque/config/SecurityConfig.java
+++ src/main/java/com/bruno/controle_estoque/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/auth/**", "/swagger-ui/**", "/v3/api-docs/**", "/webjars/**" ).permitAll()
                         .requestMatchers("/api/admin/**").hasRole("ADMIN")
                         .requestMatchers("/api/produtos/**", "/api/estoque/**").hasAnyRole("ADMIN", "GERENTE_ESTOQUE")
                         .requestMatchers("/api/viewer/**", "/api/reports/**").hasAnyRole("ADMIN", "GERENTE_ESTOQUE", "VISUALIZADOR")
